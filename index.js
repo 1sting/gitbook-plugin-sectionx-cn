01b3e35f5cbb0d2e6d0bd6aefab731a910d728fd
1sting <lishaotinglove@gmail.com>
2021-01-06 11:14:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ module.exports = {
   website: {
     assets: "./assets",
     js: [
-      "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js",
+      "https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js",
       "sectionx.js"
     ],
     css: [
-      "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css",
+      "https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css",
       "sectionx.css"
     ]
   },
